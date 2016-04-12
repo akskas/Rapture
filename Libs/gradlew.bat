9ff7cf4d05459df88116236b97f6a8bc89d35f8a
akskas <ankulkumar001@gmail.com>
2016-04-12 18:09:15 +0530

    SET

--- Libs/gradlew.bat
+++ Libs/gradlew.bat
@@ -33,6 +33,8 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
+set DEFAULT_JVM_OPTS=-Xmx512m
+set GRADLE_OPTS=-Xmx512m
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
